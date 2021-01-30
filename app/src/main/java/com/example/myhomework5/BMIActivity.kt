ec5eb3350d749d04793044a51d46b5f43ffa5b26
unknown <giorgi.natsvlishvili.1@btu.edu.ge>
2021-01-30 21:33:27 +0400

    init

---
+++ app/src/main/java/com/example/myhomework5/BMIActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myhomework5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BMIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b_m_i)
+    }
+}
